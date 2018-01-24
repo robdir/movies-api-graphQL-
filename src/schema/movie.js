722857b4f3c246f9b43db4ef7239e30675822a9e
robdir <rhwickham@gmail.com>
2018-01-24 13:25:50 +0100

    mutation types added

--- src/schema/movie.js
+++ src/schema/movie.js
@@ -1,34 +1,81 @@
 import { makeExecutableSchema } from 'graphql-tools';
 
 
-const movies = [
-    {
-    id: '666',
-    title: 'the fukkbois',
-    imdb_id: '10',
-    adult: true,
-    budget: 400000000
-},
-{
-    id: '333',
-    title: 'whov cares',
-    imdb_id: '12',
-    adult: false,
-    budget: 400000000
-}]
-
-
+// all of the graphQL definitions must be set and exported as a string
  const typeDefs = `
-    type Movie {
+    interface Media {
         id: ID!
         title: String!
+        media_type: String!
+    }
+
+    type Movie implements Media {
+        id: ID!
+        title: String!
+        media_type: String!
+        duration: Int!
+        box_office: Int!
         imbd_id: String
         adult: Boolean
-        budget: Int
+        budget(currency: Currency = EUR): Int
+        userVotes: Int
+        userRating: Int
+    }
+
+    type TVShow implements Media {
+        id: ID!
+        title: String!
+        media_type: String!
+        Episodes: [Episode]!
+        running: Boolean
+    }
+
+    type Person {
+        name: String
+        popularity: Float
+        known_for: [Media]
     }
 
     type Query {
         movies: [Movie],
+        movie(id: ID, imdb_id: String!): Movie
+        search(q: String!): SearchResult
+    }
+
+    type Mutation {
+        upvoteMovie (
+            movieId: Int!
+        ): Movie
+        rateMovie (
+            movieId: Int!
+            userRating: String!
+        ): Movie
+    }
+
+    query ReleaseYearForSearchResult($query: String!) {
+        search(q: $query) {
+            ... on Media {
+                title
+                release_date
+            }
+            ... on Person {
+                firstName
+                lastName
+                birthYear
+            }
+            ... on Company {
+                name
+                founded_on
+            }
+        }
+    }
+
+    union SearchResult = Movie | TVShow | TVShowEpisode | Company | Person
+
+    enum Currency {
+        EUR
+        GBP
+        USD
     }
  `;
 
--- src/schema/person.js
+++
@@ -1,6 +0,0 @@
-import { makeExecutableSchema } from 'graphql-tools';
-
-export const Person = `{
-    name: String
-    popularity: Float
-}`
